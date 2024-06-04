24462c9e48196f8e08dbafdafe7368d9f59ac5e9
ASUS <ASUS@DESKTOP-NB7MT4D>
2024-06-04 11:54:47 +0400

    Taskk

---
+++ 6.C#HomeWorkException/Card.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace _6.C_HomeWorkException
+{
+    public class Card
+    {
+        string Card16Num;
+        public string PIN;
+        string CVC;
+        string Expire_Date;
+        decimal Balans;
+
+        public Card(string card16Num)
+        {
+            Card16Num = card16Num;
+        }
+
+        public Card(string card16Num, string pIN, string cVC, string expire_Date, decimal balans)
+        {
+            Card16Num = card16Num;
+            PIN = pIN;
+            CVC = cVC;
+            Expire_Date = expire_Date;
+            Balans = balans;
+        }
+
+        public void ShowI()
+        {
+            Console.WriteLine($"PAN : {Card16Num}\nPIN : {PIN}\nCVC : {CVC}\nLastUseDate : {Expire_Date}\nBalans : {Balans}$\n");
+        }
+    }
+
+    public class User
+    {
+        public User(string name, string surname, Card creditcard)
+        {                                                                          
+            Name = name;                                                           
+            Surname = surname;    
+            CreditCard = creditcard;
+        }
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public Card CreditCard {  get; set; }
+
+        public void ShowI()
+        {
+            Console.Write($"Name : {Name}\nSurName : {Surname}\nCard : \n");
+            CreditCard.ShowI();
+        }
+
+    }
+    
+}
---
+++ 6.C#HomeWorkException/Program.cs
@@ -0,0 +1,43 @@
+﻿using _6.C_HomeWorkException;
+using static _6.C_HomeWorkException.User;
+Card card1 = new ("2190-9191-0120-8917","2120", "821","01/25", 3000);
+Card card2 = new ("7810-5798-7854-4112","0919", "121","02/29", 2000);
+Card card3 = new ("8576-1781-0425-0113","2229", "701","04/29", 2400);
+User user = new("Arif", "Rustamov",card1);
+User user1 = new("Fariz", "Shemseddin",card2);
+User user2 = new("Tahir", "Orxanzade",card3);
+//Card[][] cards = new Card[2][] {
+    
+//}
+Card[][] CARDS = new Card[3][];
+CARDS[0] = new Card[] { card1};
+CARDS[1] = new Card[] { card2 };
+CARDS[2] = new Card[] { card3 };
+
+while (true)
+{
+    Console.Write("PIN i Daxil Et : ");
+    string checkPin = (Console.ReadLine());
+    foreach (Card[] cardss in CARDS)
+    {
+        foreach (Card cards in cardss)
+        {
+            if (checkPin == cards.PIN)
+            {
+                Console.WriteLine("True");
+                cards.ShowI();
+                return;
+            }
+            else
+                Console.WriteLine("");
+            //return;
+        }
+    }
+}
+//card1.ShowI();
+//card2.ShowI();
+//card3.ShowI();
+//user.ShowI();   
+//user1.ShowI();   
+//user2.ShowI();   
+//if (checkPin == ) ;
